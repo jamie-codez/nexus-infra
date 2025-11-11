211b84c65210423e75ef93bc251493c53dd51976
jamie-codez <cruiseomondi90@gmail.com>
2025-11-11 12:42:10 +0300

    refactor(docker): :fire: remove legacy SFTPGo setup, redundant service definitions, and associated scripts

--- simple-dev/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is used to initialize the environment
-docker compose -f ./docker-compose.yaml build
-docker compose -f ./docker-compose.yaml --env-file ../docker/.env up -d --remove-orphans
-
-# Run this the first time you run the start script
-sudo chown -R 5050:5050 ${PWD}/../docker/pgadmin
-sudo chown -R 1000:1000 ${PWD}/../docker/sftpgo
