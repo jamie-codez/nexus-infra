1ff70fc3323a91a4ea34856e6c539eb902f85b29
jamie-codez <cruiseomondi90@gmail.com>
2025-11-16 03:16:17 +0300

    feat(docker): add multi-service configurations with health checks and centralize `.env` file management
    - Introduced `local/docker-compose.yaml` with `caddy` and `watchtower` services.
    - Added `local/databases/docker-compose.yaml` for database services and health checks.
    - Updated and centralized `.env.example` files for all services.
    - Configured `Caddyfile` for reverse proxying database admin GUIs.
    - Extended `.gitignore` to exclude redundant lock files and include specific configs.

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # This script is used to initialize the environment
-docker compose -f ./docker-compose.yaml build
 docker compose -f ./docker-compose.yaml --env-file ./.env up -d --remove-orphans
 
 # Run this the first time you run the start script
-sudo chown -R 5050:5050 ${PWD}/pgadmin
-sudo chown -R 1000:1000 ${PWD}/sftpgo
+sudo chown -R 5050:5050 "${PWD}"/databases/pgadmin
+sudo chown -R 1000:1000 "${PWD}"/media/sftpgo
